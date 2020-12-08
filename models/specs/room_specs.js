0e54d46eef2e300e7465c01bd04a9f50f2865474
Michalju <juszkin@gmail.com>
2020-12-08 16:19:40 +0000

    Created test cases for Room

---
+++ models/specs/room_specs.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const Room = require('../room');
+
+
+describe("Room class test suit:", function(){
+    let bedroom;
+
+    beforeEach(function(){
+        bedroom = new Room(32)
+    });
+
+    it("Should have an area", function(){});
+        //Arrange
+
+        //Act
+
+        //Assert
+        assert.strictEqual(bedroom.area,32);
+        beedroom.area = 52;
+        assert.strictEqual(bedroom.area,52);
+    it("Should not be being painted at the begining", function(){});
+        //Arrange
+
+        //Act
+
+        //Assert
+        assert.strictEqual(bedroom.painted,false);
+        bedroom.painted = true;
+        assert.strictEqual(bedroom.painted,true);
+
+    it("Should be able to be painted", function(){});
+        //Arrange
+
+        //Act
+
+        //Assert
+        assert.strictEqual(bedroom.toBePainted,false);
+        bedroom.toBePainted = true;
+        assert.strictEqual(bedroom.toBePainted,true);
+
+
+});
